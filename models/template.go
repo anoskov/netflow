e434f4595cadee38bc1d93abe8ef59abc0272eea
anoskov <an@gettable.ru>
2016-03-31 16:46:44 +0300

    added DecodeFlowSet() method for Template struct

--- models/template.go
+++ models/template.go
@@ -1,5 +1,9 @@
 package models
 
+import (
+	"bytes"
+)
+
 type Template struct {
 	Id 	   	uint16
 	FieldCount 	uint16
@@ -12,4 +16,36 @@ type TemplateOptions struct {
 	OptionLength	uint16
 	Scopes		[]Field
 	Options		[]Field
+}
+
+/* Methods */
+
+func (tpl *Template) DecodeFlowSet(set *DataFlow) (list []FlowData) {
+	var record FlowData
+	buf := bytes.NewBuffer(set.Data)
+
+	if set.Id != tpl.Id {
+		return
+	}
+
+
+	for i := 0; buf.Len() >= 4; i++ {
+		record.Values = extractFieldValues(buf, tpl.Fields)
+		list = append(list, record)
+	}
+
+	return
+}
+
+/* Functions */
+
+func extractFieldValues(buf *bytes.Buffer, fields []Field) (values [][]byte) {
+	values = make([][]byte, len(fields))
+	for i, f := range fields {
+		if buf.Len() < int(f.Length) {
+			break
+		}
+		values[i] = buf.Next(int(f.Length))
+	}
+	return
 }
