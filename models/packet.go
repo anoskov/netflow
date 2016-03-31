432e34ea710a25f19da30644ffe7580580335f3c
anoskov <an@gettable.ru>
2016-03-31 15:38:54 +0300

    added Templates() method for Packet struct

--- models/packet.go
+++ models/packet.go
@@ -1,11 +1,23 @@
 package models
 
 type Packet struct {
-	Version   	uint16
-	Count     	uint16
-	SysUpTime 	uint32
-	UnixSecs  	uint32
-	SequenceNumber  uint32
-	SourceId 	uint32
-	FlowSets 	[]interface{}
+	Version   		uint16
+	Count     		uint16
+	SysUpTime 		uint32
+	UnixSecs  		uint32
+	SequenceNumber  	uint32
+	SourceId 		uint32
+	FlowSets 		[]interface{}
+}
+
+func (p *Packet) Templates() (list []*Template) {
+	for i := range p.FlowSets {
+		switch set := p.FlowSets[i].(type) {
+		case TemplateFlow:
+			for j := range set.Records {
+				list = append(list, &set.Records[j])
+			}
+		}
+	}
+	return
 }
