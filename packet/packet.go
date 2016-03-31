69a9a58670e10e3fc3de845dba298435707eccba
anoskov <an@gettable.ru>
2016-03-31 12:12:03 +0300

    added packet struct

---
+++ packet/packet.go
@@ -0,0 +1,11 @@
+package packet
+
+type Packet struct {
+	Version   	uint16
+	Count     	uint16
+	SysUpTime 	uint32
+	UnixSecs  	uint32
+	SequenceNumber  uint32
+	SourceId 	uint32
+	FlowSets 	[]interface{}
+}
