0be39f02a9573fa21dd212255c8e86f787c186f8
anoskov <an@gettable.ru>
2016-03-31 15:14:45 +0300

    added template options struct

--- models/template.go
+++ models/template.go
@@ -1,7 +1,15 @@
 package models
 
 type Template struct {
-	Id uint16
-	FieldCount uint16
-	Fields []Field
+	Id 	   	uint16
+	FieldCount 	uint16
+	Fields     	[]Field
+}
+
+type TemplateOptions struct {
+	TemplateId  	uint16
+	ScopeLength 	uint16
+	OptionLength	uint16
+	Scopes		[]Field
+	Options		[]Field
 }
