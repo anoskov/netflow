aaaa1a6b663eb0efc07f2beff0bf3f3ba58135f8
anoskov <an@gettable.ru>
2016-04-01 11:19:53 +0300

    added DecodeFlowSet() method for TemplateOptions struct

--- template.go
+++ template.go
@@ -37,6 +37,22 @@ func (tpl *Template) DecodeFlowSet(set *DataFlow) (list []FlowData) {
 	return
 }
 
+func (otpl *TemplateOptions) DecodeFlowSet(set *DataFlow) (list []OptionsData) {
+	var record OptionsData
+	buf := bytes.NewBuffer(set.Data)
+
+	if set.Id != otpl.TemplateId {
+		return
+	}
+
+	for i := 0; buf.Len() >= 4; i++ {
+		record.ScopeValues  = extractFieldValues(buf, otpl.Scopes)
+		record.OptionValues = extractFieldValues(buf, otpl.Options)
+		list = append(list, record)
+	}
+	return
+}
+
 /* Functions */
 
 func extractFieldValues(buf *bytes.Buffer, fields []Field) (values [][]byte) {
